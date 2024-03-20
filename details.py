c1c79adb2f79c6a5bec9cdfb7105bb5c9ff04ab6
om705 <61368509+om705@users.noreply.github.com>
2024-03-20 21:39:21 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 22114233
 api_hash = "d7abcec5c967414fadb1d438fa05ebea"
-bot_token = "7143293335:AAEwAvxY4q77BlZSCv3fHKSZETkT_pegoMQ"
+bot_token = "7038307156:AAGVZKjcINqKRVRD_HMdywDkdUo5YGJpDrk"
 
